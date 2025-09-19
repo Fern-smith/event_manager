c58025f94e171651cd4cd69f4762b87e8f72196c
Fern Smith <USER_EMAIL>
2025-09-19 13:13:10 -0400

    testing commit

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 import { EventbriteService } from "@/lib/eventbrite";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
+
+// My Change
 
 export async function GET(request) {
   try {
