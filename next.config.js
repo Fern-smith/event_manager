85204635b4447217ea9b2dfbf64bf41cc064457a
Fern Smith <USER_EMAIL>
2025-09-30 00:41:28 -0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      },
-      {
-        protocol: "https",
-        hostname: "img.evbuc.com"
-      }
-    ]
+  webpack: (config) => {
+    config.cache = false;
+    return config;
   }
 };
 
